e62c4e2eeede04b209beb858585ab8b4f8d26aa1
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-06-27 17:01:39 -0300

    ✨  feat: add room api route

---
+++ src/pages/api/room.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { v4 as uuid } from 'uuid';
+
+import { FieldValue } from 'firebase-admin/firestore';
+
+import { HTTPMethods, StatusCode } from 'types';
+import { firestore } from 'services';
+
+const handle = async (request: NextApiRequest, response: NextApiResponse) => {
+  const method = request.method as HTTPMethods;
+
+  if (method !== HTTPMethods.POST)
+    return response
+      .status(StatusCode.METHOD_NOT_ALLOWED)
+      .json({ error: 'method not allowed' });
+
+  const id = uuid();
+
+  try {
+    await firestore
+      .doc(`room/${id}`)
+      .create({ id, created_at: FieldValue.serverTimestamp() });
+
+    response.status(StatusCode.OK).json({ roomId: id });
+  } catch (err) {
+    console.error(err);
+
+    response.status(StatusCode.INTERNAL_SERVER_ERROR).json({
+      error: 'An internal server error has occurred',
+    });
+  }
+};
+
+export default handle;
