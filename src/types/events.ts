a2e196945fd41e84c16d7a5dde8f1d9766d8eec2
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-06-28 21:45:36 -0300

    ⚡  chore: remove unused events

--- src/types/events.ts
+++ src/types/events.ts
@@ -1,8 +1,4 @@
 export enum Events {
-  PLAYER_VIDEO_PLAY = 'player.video.play',
-  PLAYER_VIDEO_PAUSE = 'player.video.pause',
-  PLAYER_VIDEO_CHANGE = 'player.video.change',
-
   PLAYER_VIDEO_PLAYED = 'player.video.played',
   PLAYER_VIDEO_PAUSED = 'player.video.paused',
   PLAYER_VIDEO_CHANGED = 'player.video.changed',
