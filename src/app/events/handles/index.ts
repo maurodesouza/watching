1dd272a05966f152a30d6a3e07009454787645e9
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-06-28 23:19:41 -0300

    ✨  feat: add modal handle events

--- src/app/events/handles/index.ts
+++ src/app/events/handles/index.ts
@@ -1,9 +1,11 @@
 import { PlayerHandleEvents } from './player';
 import { GenericHandleEvents } from './generic';
+import { ModalHandleEvents } from './modal';
 
 class Handles {
   player = new PlayerHandleEvents();
   generic = new GenericHandleEvents();
+  modal = new ModalHandleEvents();
 }
 
 export { Handles };
--- src/types/events.ts
+++ src/types/events.ts
@@ -4,4 +4,7 @@ export enum Events {
   PLAYER_VIDEO_PLAYED = 'player.video.played',
   PLAYER_VIDEO_PAUSED = 'player.video.paused',
   PLAYER_VIDEO_CHANGED = 'player.video.changed',
+
+  MODAL_OPEN = 'modal.open',
+  MODAL_CLOSE = 'modal.close',
 }
