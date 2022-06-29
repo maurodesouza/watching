ee952d3750e3454c4bfd175baa887ed9fef0e8a4
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-06-29 11:13:21 -0300

    ⚡  chore: remove unused styled component

--- src/components/player-controls/volume/styles.ts
+++ src/components/player-controls/volume/styles.ts
@@ -7,34 +7,3 @@ export const Container = styled.div`
     align-items: center;
   `}
 `;
-
-export const Range = styled.input.attrs(() => ({
-  type: 'range',
-}))`
-  ${({ theme }) => css`
-    -webkit-appearance: none;
-    cursor: grab;
-    border-radius: 1rem;
-    width: 100%;
-    height: 1rem;
-    max-width: 12rem;
-    background-size: 70% 100%;
-    background-repeat: no-repeat;
-    background-image: linear-gradient(
-      ${theme.colors.tertiary},
-      ${theme.colors.tertiary}
-    );
-
-    &:active {
-      cursor: grabbing;
-    }
-
-    &::-webkit-slider-thumb {
-      -webkit-appearance: none;
-      height: 2.4rem;
-      width: 2.4rem;
-      border-radius: 50%;
-      background: ${theme.colors.tertiary};
-    }
-  `}
-`;
