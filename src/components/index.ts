ca9fe5e0292be4a2af4e0cd44398f06632837e87
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-06-30 11:42:31 -0300

    🐛  fix: imports order

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
+export * from './inputs';
 export { Button } from './button';
 export { Player } from './player';
 export { PlayerControls } from './player-controls';
 export { ThreadListener } from './thread-listener';
-export * from './inputs';
 export { Modal } from './modals';
